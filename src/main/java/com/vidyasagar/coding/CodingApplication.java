e0b1c93f48952d8b50f86b622fc1e7ba24ea63b4
Vsk <vskushwaha01@gmail.com>
2024-05-18 14:41:18 +0530

    maven project

---
+++ src/main/java/com/vidyasagar/coding/CodingApplication.java
@@ -0,0 +1,14 @@
+package com.vidyasagar.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingApplication.class, args);
+		System.out.println("Started application......");
+	}
+
+}
