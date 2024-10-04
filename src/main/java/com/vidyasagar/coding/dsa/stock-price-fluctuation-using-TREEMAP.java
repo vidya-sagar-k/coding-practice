bae468e424fd1ec90be6e77110d0cbe1a236c11e
vidya-sagar-k <vskushwaha01@gmail.com>
2024-10-04 11:45:01 +0530

    Update stock-price-fluctuation-using-TREEMAP.java

--- src/main/java/com/vidyasagar/coding/dsa/stock-price-fluctuation-using-TREEMAP.java
+++ src/main/java/com/vidyasagar/coding/dsa/stock-price-fluctuation-using-TREEMAP.java
@@ -1,1 +1,59 @@
 
+// https://leetcode.com/problems/stock-price-fluctuation/
+
+
+class StockPrice {
+  
+    TreeMap<Integer, Integer> timePriceMap;
+    TreeMap<Integer, Integer> priceCountMap;
+    public StockPrice() {
+       
+        timePriceMap = new TreeMap<>();
+        priceCountMap = new TreeMap<>();
+
+        
+    }
+    
+    public void update(int timestamp, int price) {
+        if (timePriceMap.containsKey(timestamp)) {
+            int oldPrice = timePriceMap.get(timestamp);
+            priceCountMap.put(oldPrice, priceCountMap.get(oldPrice) - 1);
+            if (priceCountMap.get(oldPrice) == 0) {
+                priceCountMap.remove(oldPrice);
+            }
+
+        } 
+        timePriceMap.put(timestamp, price);
+        priceCountMap.put(price, priceCountMap.getOrDefault(price, 0) + 1);
+
+
+        
+    }
+    
+    public int current() {
+        if (!timePriceMap.isEmpty()) {
+            return timePriceMap.lastEntry().getValue();
+        }
+        
+        return -1;
+    }
+    
+    public int maximum() {
+        return priceCountMap.lastKey();
+        
+    }
+    
+    public int minimum() {
+        return priceCountMap.firstKey();
+        
+    }
+}
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * StockPrice obj = new StockPrice();
+ * obj.update(timestamp,price);
+ * int param_2 = obj.current();
+ * int param_3 = obj.maximum();
+ * int param_4 = obj.minimum();
+ */
