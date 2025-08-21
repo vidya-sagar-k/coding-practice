045abdce603f69ce946278b9bc3a92129f7580e9
vidya-sagar-k <vskushwaha01@gmail.com>
2025-08-21 11:38:50 +0530

    Update longest-palindromic-substring.java
    v2 aug 2025

--- src/main/java/com/vidyasagar/coding/dsa/longest-palindromic-substring.java
+++ src/main/java/com/vidyasagar/coding/dsa/longest-palindromic-substring.java
@@ -32,4 +32,32 @@ class Solution {
         return s.substring(startIndex, endIndex+1);
         
     } 
+
+    public String longestPalindromeV2(String s) {
+        int l = s.length();
+
+        if (l<=1) return s;
+        
+        int start = 0, end = 0;
+
+        int[][] dp = new int[l][l];
+        for (int i = 0; i<l; i++) dp[i][i] = 1;
+
+        for (int k = 2; k<=l; k++) {
+            for (int i = 0; i<= l - k; i++) {
+                int j = i + k-1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    // same
+                    if (i+1 >= j-1 || dp[i+1][j-1] == 1) {
+                        dp[i][j] = 1;
+                        
+                        end = j;
+                        start = i;
+                    }
+                }
+            }
+        }
+
+        return s.substring(start, end+1);
+    }
 }
