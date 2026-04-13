525b5317d394d648cd055c2d013afa3d1c5e08d7
vidya-sagar-k <vskushwaha01@gmail.com>
2026-04-13 07:11:30 +0530

    Update min-height-trees.java

--- src/main/java/com/vidyasagar/coding/dsa/topological-sorting/min-height-trees.java
+++ src/main/java/com/vidyasagar/coding/dsa/topological-sorting/min-height-trees.java
@@ -1,8 +1,59 @@
 class Solution {
-
-
-  // Brute force approach - 
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+      // optimal solution, start removing LEAF nodes, the last remaing 2 or less nodes are the roots
+        if (edges.length == 0) return Arrays.asList(0);
+        HashMap<Integer, List<Integer>> adj = new HashMap<>();
+        int[] inDegree = new int[n];
+
+        for (int[] e : edges) {
+            int a = e[0];
+            int b = e[1];
+
+            inDegree[a]++;
+            inDegree[b]++;
+
+            adj.computeIfAbsent(a, k-> new ArrayList()).add(b);
+            adj.computeIfAbsent(b, k-> new ArrayList()).add(a);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i<n; i++) {
+            if (inDegree[i] == 1) {
+                q.add(i);
+            }
+        }
+
+        int pruned = 0;
+        
+        while(!q.isEmpty()) {
+            if (n - pruned <= 2) {
+                break;
+            }
+
+            int size = q.size();
+            for(int i = 0; i<size; i++) {
+                int curr = q.remove();
+                pruned++;
+                for (int nbr : adj.get(curr)) {
+                    inDegree[nbr]--;
+                    if (inDegree[nbr] == 1) {
+                        q.add(nbr);
+                    }
+                }
+            }
+        }
+
+        // return list of non prune 1, 2 nodes
+        List<Integer> ans = new ArrayList<>();
+         while(!q.isEmpty()) {
+            ans.add(q.remove());
+         }
+         return ans;
+
+
+
+    }
+    public List<Integer> findMinHeightTreesBruteForce(int n, int[][] edges) {
         if (edges.length == 0) return Arrays.asList(0);
         HashMap<Integer, List<Integer>> adj = new HashMap<>();
 
